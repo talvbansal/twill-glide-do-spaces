2e2521357450f1e256ae2c34278adcb9fad5db77
Talv Bansal <talvbansal@outlook.com>
2020-07-23 21:24:21 +0100

    Remove un-needed cruft

--- src/TwillGlideDoSpacesServiceProvider.php
+++ src/TwillGlideDoSpacesServiceProvider.php
@@ -2,12 +2,8 @@
 
 namespace Talvbansal\TwillGlideDoSpaces;
 
-use A17\Twill\Repositories\FileRepository;
-use A17\Twill\Repositories\MediaRepository;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
-use Talvbansal\TwillGlideDoSpaces\Controllers\ImageController;
 
 class TwillGlideDoSpacesServiceProvider extends ServiceProvider
 {
@@ -18,9 +14,6 @@ class TwillGlideDoSpacesServiceProvider extends ServiceProvider
                 __DIR__.'/../config/twill-glide-do-spaces.php' => config_path('twill-glide-do-spaces.php'),
             ], 'config');
         }
-
-        // image routes...
-        Route::get('/' . config('twill.glide.base_path') . '/{path}', [ImageController::class, 'show'])->where('path', '.*');
     }
 
     public function register()
@@ -29,36 +22,16 @@ class TwillGlideDoSpacesServiceProvider extends ServiceProvider
 
         // if the glide sources are set to disk names lets fetch the flysystem driver for glide to use...
         if ($this->isDisk(env('GLIDE_SOURCE', ''))) {
-            $diskName = env('GLIDE_SOURCE', 'do_spaces');
-            config()->set('twill.glide.source', Storage::disk($diskName)->getDriver());
-            config()->set('twill.glide.base_url', config('filesystems.disks.'.$diskName.'.endpoint'));
+            config()->set('twill.glide.source', Storage::disk(env('GLIDE_SOURCE', 'do_spaces'))->getDriver());
         }
 
         if ($this->isDisk(env('GLIDE_CACHE', ''))) {
             config()->set('twill.glide.cache', Storage::disk(env('GLIDE_CACHE', 'do_spaces'))->getDriver());
         }
-
-        // Twill creates 2 new disks on the fly, here we set a new root for files uploaded by the media and file uploaders when an s3 driver is in use...
-        if ($this->usesS3Driver(config('filesystems.disks.twill_file_library'))) {
-            config()->set('filesystems.disks.twill_file_library.root', env('DO_SPACES_FILES_ROOT', 'files/'));
-        }
-
-        if ($this->usesS3Driver(config('filesystems.disks.twill_media_library'))) {
-            config()->set('filesystems.disks.twill_media_library.root', env('DO_SPACES_IMAGES_ROOT', 'img/'));
-        }
-
-        // Overwrite Twill's File Repo with our version until this is patched...
-        $this->app->bind(FileRepository::class, \Talvbansal\TwillGlideDoSpaces\Repositories\FileRepository::class);
-        $this->app->bind(MediaRepository::class, \Talvbansal\TwillGlideDoSpaces\Repositories\MediaRepository::class);
     }
 
     private function isDisk(string $diskName = ''): bool
     {
         return array_key_exists($diskName, config('filesystems.disks'));
-    }
-
-    public function usesS3Driver(array $disk): bool
-    {
-        return ($disk['driver'] === 's3') ?? false;
     }
 }
